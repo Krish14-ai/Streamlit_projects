cd78b805257b21ac13d6eecd400a1ad733983518
krish14-ai <kkhandelwal292@gmail.com>
2026-08-21 23:58:57 +0530

    we writing code my self

--- Introduction/widgits.py
+++ Introduction/widgits.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import time
 
 st.title("Bevrege Maker App")
 st.subheader("Choose what you want")
@@ -10,9 +11,9 @@ if bev == "Coffee" :
 
     st.write("What kind of Coffee you want")
 
-    type = st.radio("Choose what you want",["Espresso","Black","Latte","Cappuccino","Custom"])
+    coffee_type = st.radio("Choose what you want",["Espresso","Black","Latte","Cappuccino","Custom"])
 
-    if type == "Custom":
+    if coffee_type == "Custom":
 
         milk = st.checkbox("Milk")
         cream = st.checkbox("Cream")
@@ -35,7 +36,8 @@ if bev == "Coffee" :
             st.success("Bon Appétit !")
 
     else : 
-        st.write(f"Your {type} is ready")
+        st.write(f"Your {coffee_type} is ready")
+        time.time.sleep(2)
         st.success("Bon Appétit")
 
 ##-------------------------------## Chai ##--------------------------------------
@@ -43,30 +45,4 @@ if bev == "Chai" :
 
     st.write("What kind of Chai you want")
 
-    type = st.radio("Choose what you want",["Ginger Chai","Elaichi Chai","Masala Chai","Adrak-Elaichi Chai","Custom"])
-
-    if type == "Custom":
-
-        milk = st.checkbox("Milk")
-        cream = st.checkbox("Cream")
-        sugar = st.checkbox("Sugar")
-        choco = st.checkbox("Choco Powder")
-
-        
-
-        if st.button("Make Chai"):
-            if milk :
-                st.write("Added Milk")
-            if cream :
-                st.write("Added Cream")
-            if sugar :
-                st.write("Added Sugar")
-            if choco :
-                st.write("Added Chocolate")
-
-            st.write("Chai is ready")
-            st.success("Bon Appétit !")
-
-    else : 
-        st.write(f"Your {type} is ready")
-        st.success("Bon Appétit")
+    chai_type = st.
