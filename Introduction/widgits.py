950784551e440c48d60a9f3b21f4c1354f93a107
krish14-ai <kkhandelwal292@gmail.com>
2026-08-21 23:54:39 +0530

    added chai using Selection Box

--- Introduction/widgits.py
+++ Introduction/widgits.py
@@ -1,34 +1,39 @@
 import streamlit as st
 
-st.title("Coffee Maker App")
+st.title("Bevrege Maker App")
 st.subheader("Choose what you want")
 
-st.write("What kind of Coffee you want")
+bev = st.selectbox("Select what you want to drink",["Chai", "Coffee"])
 
-type = st.radio("Choose what you want",["Espresso","Black","Latte","Cappuccino","Custom"])
+##-------------------------------## Coffee ##--------------------------------------
+if bev == "Coffee" :
 
-if type == "Custom":
+    st.write("What kind of Coffee you want")
 
-    milk = st.checkbox("Milk")
-    cream = st.checkbox("Cream")
-    sugar = st.checkbox("Sugar")
-    choco = st.checkbox("Choco Powder")
+    type = st.radio("Choose what you want",["Espresso","Black","Latte","Cappuccino","Custom"])
 
-    
+    if type == "Custom":
 
-    if st.button("Make Coffee"):
-        if milk :
-            st.write("Added Milk")
-        if cream :
-            st.write("Added Cream")
-        if sugar :
-            st.write("Added Sugar")
-        if choco :
-            st.write("Added Chocolate")
+        milk = st.checkbox("Milk")
+        cream = st.checkbox("Cream")
+        sugar = st.checkbox("Sugar")
+        choco = st.checkbox("Choco Powder")
 
-        st.write("Coffee is ready")
-        st.success("Bon Appétit !")
+        
 
-else : 
-    st.write(f"Your {type} is ready")
-    st.success("Bon Appétit")
+        if st.button("Make Coffee"):
+            if milk :
+                st.write("Added Milk")
+            if cream :
+                st.write("Added Cream")
+            if sugar :
+                st.write("Added Sugar")
+            if choco :
+                st.write("Added Chocolate")
+
+            st.write("Coffee is ready")
+            st.success("Bon Appétit !")
+
+    else : 
+        st.write(f"Your {type} is ready")
+        st.success("Bon Appétit")
