3f2bdbfdf8b3bbcf926ead05b2c7a6eab3233186
krish14-ai <kkhandelwal292@gmail.com>
2026-08-22 01:26:45 +0530

    Completed :)

--- Introduction/widgits.py
+++ Introduction/widgits.py
@@ -2,78 +2,91 @@ import streamlit as st
 import time
 
 st.title("Bevrege Maker App")
-st.subheader("Choose what you want")
+st.subheader("Choose what Makes you Happy :)")
 
-bev = st.selectbox("Select what you want to drink",["Chai", "Coffee"])
+name = st.text_input("Enter your name : ")
 
-##-------------------------------## Coffee ##--------------------------------------
-if bev == "Coffee" :
+if name:
 
-    st.write("What kind of Coffee you want")
+    st.write(f"Welcome {name}")
+    
+    bev = st.selectbox("Select what you want to drink",["Chai", "Coffee"])
 
-    coffee_type = st.radio("Choose what you want",["Espresso","Black","Latte","Cappuccino","Custom"])
+    ##-------------------------------## Coffee ##--------------------------------------
+    if bev == "Coffee" :
 
-    if coffee_type == "Custom":
+        st.write("What kind of Coffee you want")
 
-        ## Check boxes
-        milk = st.checkbox("Milk")
-        cream = st.checkbox("Cream")
-        sugar = st.checkbox("Sugar")
-        choco = st.checkbox("Choco Powder")
+        coffee_type = st.radio("Choose what you want",["Espresso","Black","Latte","Cappuccino","Custom"])
 
-        if sugar:
-            sug_cubes= st.slider("Amount of Sugar Cubes you prefer", 0,5,2)
-        
-        ## Buttons
-        if st.button("Brew Coffee"):
-            if milk :
-                st.write("Added Milk")
-            if cream :
-                st.write("Added Cream")
-            if sugar :
-                st.write(f"Added {sug_cubes} cubes of Sugar")
-            if choco :
-                st.write("Added Chocolate")
+        if coffee_type == "Custom":
 
-            st.write("Coffee is ready")
-            st.success("Bon Appétit !")
+            ## Check boxes
+            milk = st.checkbox("Milk")
+            cream = st.checkbox("Cream")
+            sugar = st.checkbox("Sugar")
+            choco = st.checkbox("Choco Powder")
 
-    else : 
-        if st.button("Brew Coffee"):
-            st.write(f"Your {coffee_type} is ready")
-            st.success("Bon Appétit")
-
-##-------------------------------## Chai ##--------------------------------------
-if bev == "Chai" :
-
-    st.write("What kind of Chai you want")
-
-    chai_type = st.radio("Choose what tea you want",["Normal Tea", "Ginger", "Green","Macha","Rose","Masala","Tulsi","Custom"])
-
-    if chai_type is not "Custom":
-        if st.button("Brew Tea"):
-            st.write(f"Your {chai_type} is ready")
-            st.success("Bon Appétit")
-
-    if chai_type == "Custom":
-        sugar = st.checkbox("Sugar")
-        Ginger = st.checkbox("Ginger")
-        Masala = st.checkbox("Masala")
-        Milk  =  st.checkbox("Milk")
-
-        if sugar :
-            sug_cubes = st.slider("Amount of Sugar Cubes you prefer", 0,5,2)
-
-        if st.button("Brew Chai"):
             if sugar:
-                st.write(f"Added {sug_cubes} Cubes of Sugar")
-            if Ginger:
-                st.write("Added Ginger")
-            if Milk :
-                st.write("Added Milk")
-            if Masala : 
-                st.write("Added Masala")
-            st.write("Your Chai is Ready!")
-            st.success("Bon Appétit")
+                sug_cubes= st.slider("Amount of Sugar Cubes you prefer", 0,5,2)
+            
+            ## Buttons
+            if st.button("Brew Coffee"):
+                if milk :
+                    st.write("Added Milk")
+                if cream :
+                    st.write("Added Cream")
+                if sugar :
+                    st.write(f"Added {sug_cubes} cubes of Sugar")
+                if choco :
+                    st.write("Added Chocolate")
+
+                st.write("Coffee is ready")
+                st.success("Bon Appétit !")
+
+        else : 
+            if st.button("Brew Coffee"):
+                st.write(f"Your {coffee_type} is ready")
+                st.success("Bon Appétit")
+
+    ##-------------------------------## Chai ##--------------------------------------
+    if bev == "Chai" :
+
+        st.write("What kind of Chai you want")
+
+        chai_type = st.radio("Choose what tea you want",["Normal Tea", "Ginger", "Green","Macha","Rose","Masala","Tulsi","Custom"])
+
+        cups = st.number_input("How many cups of Chai you want",1,10,1)
+
+        if chai_type != "Custom":
+            if st.button("Brew Tea"):
+                st.write(f"Your {cups} cups of {chai_type} tea are Ready!")
+                st.success("Bon Appétit")
+
+        
+
+        if chai_type == "Custom":
+            sugar = st.checkbox("Sugar")
+            Ginger = st.checkbox("Ginger")
+            Masala = st.checkbox("Masala")
+            Milk  =  st.checkbox("Milk")
+
+            if sugar :
+                sug_cubes = st.slider("Amount of Sugar Cubes you prefer", 0,5,2)
+
+            if st.button("Brew Chai"):
+                if sugar:
+                    st.write(f"Added {sug_cubes} Cubes of Sugar")
+                if Ginger:
+                    st.write("Added Ginger")
+                if Milk :
+                    st.write("Added Milk")
+                if Masala : 
+                    st.write("Added Masala")
+        
+                st.write(f"Your {cups} cups of Chai are Ready!")
+                st.success("Bon Appétit")
+    
+
 
 
