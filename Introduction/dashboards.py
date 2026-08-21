3ce716555583a0ac931f3040a172ea0f27a1f888
krish14-ai <kkhandelwal292@gmail.com>
2026-08-22 01:45:08 +0530

    Done for today

--- Introduction/dashboards.py
+++ Introduction/dashboards.py
@@ -12,3 +12,8 @@ with col2:
     st.header("Ginger Chai")
     vote2 = st.button("Vote Ginger Chai")
 
+if col1.button("Vote Masala Chai"):
+    st.write("You voted for Masala Chai")
+
+if col2.button("Vote Ginger Chai"):
+    st.write("You voted for Ginger Chai")
