d662d1e6fc527e32932c430e4bbb7a46634222d8
krish14-ai <kkhandelwal292@gmail.com>
2026-08-21 23:48:06 +0530

    Learning radio widget

--- Introduction/widgits.py
+++ Introduction/widgits.py
@@ -3,20 +3,29 @@ import streamlit as st
 st.title("Coffee Maker App")
 st.subheader("Choose what you want")
 
-cream = st.checkbox("Cream")
-sugar = st.checkbox("Sugar")
-choco = st.checkbox("Chocolate")
+st.write("What kind of Coffee you want")
+
+type = st.radio("Choose what you want",["Espresso","Black","Latte","Cappuccino","Custom"])
+
+if type == "Custom":
+
+    milk = st.checkbox("Milk")
+    cream = st.checkbox("Cream")
+    sugar = st.checkbox("Sugar")
+    choco = st.checkbox("Choco Powder")
 
 
-if st.button("Make Coffee"):
-    if cream :
-        st.write("Added Cream")
-    if sugar :
-        st.write("Added Sugar")
-    if choco :
-        st.write("Added Choco")
+    if st.button("Make Coffee"):
+        if milk :
+            st.write("Added Milk")
+        if cream :
+            st.write("Added Cream")
+        if sugar :
+            st.write("Added Sugar")
+        if choco :
+            st.write("Added Chocolate")
 
-    st.write("Coffee is ready")
-    st.success("bon appétit !")
+        st.write("Coffee is ready")
+        st.success("bon appétit !")
 
 
