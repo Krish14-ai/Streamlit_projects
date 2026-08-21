a64fa34fff68dfd5d5f9dcdf96804e66d1ae71c3
krish14-ai <kkhandelwal292@gmail.com>
2026-08-21 23:41:26 +0530

    Making it

--- Introduction/widgits.py
+++ Introduction/widgits.py
@@ -1,4 +1,16 @@
 import streamlit as st
 
-st.title("Press the Button")
-st.button("Click me")
+st.title("Coffee Maker App")
+st.subheader("Choose what you want")
+
+cream = st.checkbox("Cream")
+sugar = st.checkbox("Sugar")
+choco = st.checkbox("Chocolate")
+
+st.write(sugar, choco)
+if st.button("Make Coffee"):
+    st.write(f"Added {cream}")
+    st.write("Coffee is ready")
+    st.success("bon appétit !")
+
+
