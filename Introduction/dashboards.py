0f8868533723b8c706adcb6cafd3deba4bf9b5a6
krish14-ai <kkhandelwal292@gmail.com>
2026-08-22 14:56:38 +0530

    Added images

--- Introduction/dashboards.py
+++ Introduction/dashboards.py
@@ -6,14 +6,17 @@ col1, col2= st.columns(2)
 
 with col1:
     st.header("Masala Chai")
+    st.image("https://www.indianhealthyrecipes.com/wp-content/uploads/2023/05/indian-masala-chai-tea.webp", width = 200)
     vote1 = st.button("Vote Masala Chai")
 
 with col2:
     st.header("Ginger Chai")
+    st.image("https://i0.wp.com/dailyteatime.com/wp-content/uploads/2021/04/ginger-chai-12001800.jpg?resize=1024%2C1536&ssl=1", width = 200)
     vote2 = st.button("Vote Ginger Chai")
 
-if col1.button("Vote Masala Chai"):
+if vote1:
     st.write("You voted for Masala Chai")
 
-if col2.button("Vote Ginger Chai"):
+if vote2:
     st.write("You voted for Ginger Chai")
+    
