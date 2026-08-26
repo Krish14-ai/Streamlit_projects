2086bf2fa3da57a738aa3d12ff3f6d33ab68e1bd
krish14-ai <kkhandelwal292@gmail.com>
2026-08-26 23:41:42 +0530

    completed the skeleton of frontend

--- Marks_Pridector/app/frontend/front.py
+++ Marks_Pridector/app/frontend/front.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import numpy as np
 st.title("Welcome to the Placement Predictor")
 
 name = st.text_input("Enter Your name : ")
@@ -8,3 +8,7 @@ marks = st.number_input("Enter your marks : ")
 internals = st.number_input("Enter your Internal Marks : ")
 papers = st.number_input("Total Research Papers published : ")
 projects = st.number_input("Total no of Projects submitted : ")
+
+if st.button("Submit"):
+    l = np.array([age,marks,internals,papers,projects])
+    st.success(f"{l}")
