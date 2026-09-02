023a786f1272025922ed3eb37244a6b9cc464166
krish14-ai <kkhandelwal292@gmail.com>
2026-09-03 01:12:34 +0530

    Going to sleep

--- Projects/Spam_Classifier/model/classifier.py
+++ Projects/Spam_Classifier/model/classifier.py
@@ -1,4 +1,8 @@
 import pandas as pd 
 from sklearn.ensemble import RandomForestClassifier
 
-df  = 
+from pathlib import Path
+data_path = Path(__file__).resolve().parent.parent/"data"/"combined_data.csv"
+
+df  = pd.read_csv(data_path)
+print(df)
